af2dd075457e8a6fbd1ee6815c477e426098c710
Mythri Devulapally <51511650+mythrinetha@users.noreply.github.com>
2020-05-28 17:51:36 +0530

    Create Address.java

---
+++ Customer-Address Inner Bean/Address.java
@@ -0,0 +1,39 @@
+package com.spring.app;
+
+public class Address {
+	private String houseName,street,city,state;
+	
+	public void setHouseName(String s){
+	    houseName=s;
+	}
+	public String getHouseName(){
+	    return houseName;
+	}
+	
+	
+	public void setStreet(String s){
+	    street=s;
+	}
+	public String getStreet(){
+	    return street;
+	}
+	
+	
+	public void setCity(String s){
+	   city =s;
+	}
+	public String getCity(){
+	    return city;
+	}
+	
+	
+	public void setState(String s){
+	   state =s;
+	}
+	public String getState(){
+	    return state;
+	}
+
+
+
+}
